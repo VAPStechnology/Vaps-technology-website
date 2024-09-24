2579654d756add8478181da41d4f598e253babfd
Pkamal2002 <135655190+Pkamal2002@users.noreply.github.com>
2024-09-24 16:21:22 +0530

    fix/metatag

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,8 +4,8 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Contact Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Contact Page for Startup Nextjs Template",
+  title: "Contact Page | VAPS technology",
+  description: "we create and design website for your business",
   // other metadata
 };
 
